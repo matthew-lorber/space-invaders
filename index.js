57f3353486e4c8696f3e7d3fdecd16dce1759129
Matt Lorber <matthew.lorber@outlook.com>
2020-01-29 20:52:55 -0600

    wednesday updates: movement

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-$(window).on("load",function(){
-    $()
-})
+let keys = [];
+
+$(document).keydown(function(e){
+        
+    if (e.keyCode === 65 || e.keyCode === 68) {
+        keys.push(e.keyCode);
+        keying();
+    }
+
+//$("#player").stop(true,false).animate({"left":"+=100vw"},4000,"linear");
+});
+
+function keying() {
+    for (let i=0; i<keys.length; i++) {
+        if (keys[i] === 65) {
+            $("#player").stop(true,false).animate({"left":"-=100vw"},4000,"linear");
+        } else {
+            $("#player").stop(true,false).animate({"left":"+=100vw"},4000,"linear");
+        }
+    }
+    resetKeys();
+}
+
+function resetKeys() {
+    keys = [];
+}
